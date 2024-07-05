9b46d8c9346ca32099d56029cd61bcc7bd450d4f
MarcosHenriqueFr <161951682+MarcosHenriqueFr@users.noreply.github.com>
2024-07-05 12:30:02 -0300

    exercicios

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80.05
+console.log(`A velocidade atual do carro é: ${vel}Km/h!`)
+
+if (vel > 80) {//condição simples
+    console.log('Está acima da velocidade indicada')
+}
---
+++ aula11/ex010.js
@@ -0,0 +1,8 @@
+var país = 'Brasil'//em js pode acento
+console.log(`Vivendo em ${país}`)
+
+if (país == 'Brasil') {
+    console.log('Brasileiro')
+} else {
+    console.log('Estrangeiro')
+}
