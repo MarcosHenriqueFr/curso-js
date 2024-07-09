aec64d5add57fc188ced83330316e2126281f8cd
MarcosHenriqueFr <161951682+MarcosHenriqueFr@users.noreply.github.com>
2024-07-09 15:27:46 -0300

    laços

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+//var i = 1
+
+/*
+while (i < 6) {
+    console.log(`Olá tudo bem? ${i}`)
+    i++
+}
+*/
+/*
+do {
+    console.log(`Eu tenho ${i} anos`)
+    i++
+} while (i <= 0)
+*/
+for(var i = 1; i<=5; i++) {
+    console.log(`Passo ${i}`)
+}
