1019e3e7d650e065d713fb94b5f0c42fc8752e59
MarcosHenriqueFr <161951682+MarcosHenriqueFr@users.noreply.github.com>
2024-07-10 17:04:56 -0300

    testes iterações

---
+++ ref1/conteudo-video/aula14ex/ex016-1/script.js
@@ -0,0 +1,23 @@
+var botao = document.querySelector('input#botao')
+var ativar = botao.addEventListener('click', contador)
+
+function contador(){
+    var txtvar = document.querySelector('input#txtvar')
+    var txtcond = document.querySelector('input#txtcond')
+    var txtinc = document.querySelector('input#txtinc')
+    var res = document.querySelector('div#res')
+
+    res.innerHTML = `<p>Contando: </p>`
+
+    var exp = Number(txtvar.value)
+    var condicao = Number(txtcond.value)
+    var cont = Number(txtinc.value)
+
+    while (exp <= condicao) {
+        res.innerHTML += `${exp} &#x1F449; `
+        exp+=cont
+    }
+    
+    res.innerHTML += `&#x1F3C1;`
+}
+
