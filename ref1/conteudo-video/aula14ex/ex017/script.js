0911a789340b09607e15ac9904d3404db18127cc
MarcosHenriqueFr <161951682+MarcosHenriqueFr@users.noreply.github.com>
2024-07-11 18:07:19 -0300

    fim aula14ex

--- ref1/conteudo-video/aula14ex/ex017/script.js
+++ ref1/conteudo-video/aula14ex/ex017/script.js
@@ -1,1 +1,23 @@
-//window.alert('Olá') o js externo
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    tab.style.textAlign = 'center'
+
+    if(num.value.length == 0){
+        window.alert('[ERRO] Digite um número')
+    } else {
+        let n = Number(num.value) 
+        let c = 1
+
+        tab.innerHTML = ``//limpa e mostra a próxima tabuada
+
+        while (c <= 10) {
+            let item = document.createElement('option')//options para o select funcionar, com values
+            item.text = `${n} x ${c} = ${n * c}`//modificação do texto do item
+            item.value = `tab${c}`//para a identificação dentro do elemento select
+            tab.appendChild(item)//criamos o item e adicionamos 
+            c++
+        }
+    }
+    
+}
