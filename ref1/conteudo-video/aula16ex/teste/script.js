8ae664807d39443c78bcac85724ab0c4c5c4bfb4
MarcosHenriqueFr <161951682+MarcosHenriqueFr@users.noreply.github.com>
2024-07-12 21:27:26 -0300

    inicio ex18

--- ref1/conteudo-video/aula16ex/teste/script.js
+++ ref1/conteudo-video/aula16ex/teste/script.js
@@ -1,1 +1,23 @@
-//window.alert('Olá') o js externo
+//config da function de adicionar números
+function addnum(){
+    //config da caixa de input, para receber o valor dela
+    var txtn = document.getElementById('txtn')
+    var num = Number(txtn.value)
+
+    var item = document.createElement('option')
+    item.text = `Valor ${num} adicionado`
+    return tabela.appendChild(item)
+}
+
+//config do botão
+var btn1 = document.getElementById('btn1')
+btn1.addEventListener('click', addnum)
+
+//local da div res
+var res = document.getElementById('res')
+
+//localização da tabela do select
+var tabela = document.getElementById('tabela')
+
+var dados = [addnum()]
+res.innerHTML += `${dados}`
