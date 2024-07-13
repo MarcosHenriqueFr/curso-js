d61723ae696f526c9b1e90edba55ccf916718def
MarcosHenriqueFr <161951682+MarcosHenriqueFr@users.noreply.github.com>
2024-07-13 10:57:36 -0300

    modificação teste

--- ref1/conteudo-video/aula16ex/teste/script.js
+++ ref1/conteudo-video/aula16ex/teste/script.js
@@ -1,23 +1,47 @@
-//config da function de adicionar números
-function addnum(){
-    //config da caixa de input, para receber o valor dela
-    var txtn = document.getElementById('txtn')
-    var num = Number(txtn.value)
 
-    var item = document.createElement('option')
-    item.text = `Valor ${num} adicionado`
-    return tabela.appendChild(item)
+//config da function de adicionar números
+function addnum()
+{
+//config da caixa de input, para receber o valor dela
+var txtn = document.getElementById('txtn')
+var num = Number(txtn.value)
+
+    if(txtn.value.length == 0) {
+        alert("[ERRO] Valor não foi inserido")
+    } else if (num < 0 | num > 100) {
+        alert('[ERRO] Valor inserido está fora do escopo')
+    } else {
+        var item = document.createElement('option')
+        item.text = `Valor ${num} adicionado`
+        tabela.appendChild(item)
+        
+        guardaritem(num)//chama a função de guardar item
+    }
 }
 
-//config do botão
+//guarda o nosso valor na última posição da array
+function guardaritem(valor) {
+    banco.push(valor)
+}
+
+//config da função de resultado
+function dados() {
+    res.innerHTML += `<p>${banco}</p>`
+}
+
+//config da array que guarda os valores
+var banco = []
+
+//config dos botões
 var btn1 = document.getElementById('btn1')
 btn1.addEventListener('click', addnum)
+
+var btn2 = document.getElementById('btn2')
+btn2.addEventListener('click', dados)
 
 //local da div res
 var res = document.getElementById('res')
 
-//localização da tabela do select
+//local da tabela do select
 var tabela = document.getElementById('tabela')
 
-var dados = [addnum()]
-res.innerHTML += `${dados}`
