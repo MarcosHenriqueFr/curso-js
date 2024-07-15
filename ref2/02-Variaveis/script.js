5f9ed57a7f7b32a5a919b0a2bdea7ee229ee9cb7
MarcosHenriqueFr <161951682+MarcosHenriqueFr@users.noreply.github.com>
2024-07-15 18:28:51 -0300

    inicio da referência2

---
+++ ref2/02-Variaveis/script.js
@@ -0,0 +1,7 @@
+let nome = "Marcos Henrique";
+let idade = 18;
+let Estudante = true;
+
+document.getElementById("p1").textContent = `Nome do usuário: ${nome}`
+document.getElementById("p2").textContent = `Idade do usuário: ${idade} anos`;
+document.getElementById("p3").textContent = `O usuário é um estudante: ${Estudante}`
---
+++ ref2/02-Variaveis/script1.js
@@ -0,0 +1,14 @@
+let x = 10;
+let nome = "Marcos";
+let comidaFav = "Pizza";
+let online = true;
+
+
+console.log(typeof comidaFav);
+console.log(`Sua comida favorita é: ${comidaFav}!!`);
+console.log(typeof nome);
+console.log(`Meu nome é: ${nome}`);
+console.log(typeof online);
+console.log(`O usuário ${nome} está online? ${!online}`);//geralmente não são usados como dados de saída, mais usados em ifs
+// console.log(typeof x);
+// console.log(x);
