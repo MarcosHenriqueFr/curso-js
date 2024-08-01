4ad9595e97567ef9433ca79a547361977adc0d73
MarcosHenriqueFr <161951682+MarcosHenriqueFr@users.noreply.github.com>
2024-08-01 18:11:28 -0300

    ajustes

--- ref2/28-DiceRollerProgram/script.js
+++ ref2/28-DiceRollerProgram/script.js
@@ -2,7 +2,7 @@ function rolarDados(){
     const resultImage = document.getElementById("resultImage");
     const resultValue = document.getElementById("resultValue");
     const textBox = document.getElementById("textBox");
-    let value = [];
+    let valores = [];
     let imagens = [];
 
     if(textBox.value == ""){
@@ -11,12 +11,12 @@ function rolarDados(){
         resultValue.textContent = "";
 
         for(let i = 0; i < textBox.value; i++){
-            value[i] = Math.floor(Math.random() * 6 + 1);
+            valores[i] = Math.floor(Math.random() * 6 + 1);
             //dá para colocar a tag toda, ao invés de criar dinamicamente, com appendchil
-            imagens.push(`<img src="./dado-img/0${value[i]}.png" alt="Dado ${value[i]}">`);
+            imagens.push(`<img src="./dado-img/0${valores[i]}.png" alt="Dado ${valores[i]}">`);
         }
 
-        resultValue.innerHTML = `Lados: ${value.join(", ")}`;
+        resultValue.innerHTML = `Lados: ${valores.join(", ")}`;
         resultImage.innerHTML = `Imagens: <br>${imagens.join(" ")}`;
     }
 }
