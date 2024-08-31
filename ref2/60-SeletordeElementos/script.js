8057418bb0ab222f9bb76585b4d2483261f68812
Marcos Henrique <161951682+MarcosHenriqueFr@users.noreply.github.com>
2024-08-31 20:05:57 -0300

    Seleção de elementos do DOM

---
+++ ref2/60-SeletordeElementos/script.js
@@ -0,0 +1,10 @@
+//métodos usados para alcançar e manipular os elementos HTML
+//É possível selecionar um ou vários do DOM
+//A mesma coisa de antes
+//os que usam index retornam coleções de HTML
+
+//HTML COLLECTION - FOR OF , Não é tão utilizável
+//array.from(HTML COLLECTION) - são dinâmicos  
+
+//querySelector - primeiro elemento ou null
+//querySelectorAll - NODELIST - tem métodos em si, eles são estáticos
