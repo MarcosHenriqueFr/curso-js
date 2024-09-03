5ff3ab0858532339a7f1233bdc6fc1f36261994d
Marcos Henrique <161951682+MarcosHenriqueFr@users.noreply.github.com>
2024-09-03 12:18:32 -0300

    json, fetch data

--- ref2/73-JSON/script.js
+++ ref2/73-JSON/script.js
@@ -3,8 +3,23 @@
 //JSON.parse() = faz o exato inverso, string json para objeto em js
 //fetch("url").then(reposta => resposta.json()).then(valor => console.log(valor))//retorna uma promise
 
+/* 
 fetch("pessoas.json").then(resposta => resposta.json()).then(valores => valores.forEach(elemento => {
     console.log(elemento.name);
 })).catch(error => {
     console.log(error);
+});
+*/
+//fetch("nome do arquivo").then(uma função para abrir o arquivo).then(uma função que procura a informação dentro do arquivo)
+
+fetch("pessoa.json").then(arquivo => arquivo.json()).then(valor => console.log(valor.name));
+
+fetch("pessoas.json").then(arquivo => arquivo.json()).then(valores => {
+    let novosValores = valores.filter(elemento => {
+        return elemento.isEmployed == true;
+    });
+    console.log(novosValores);
+    valores.forEach(elemento => {
+        console.log(elemento);
+    });
 });
